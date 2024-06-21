a2367d9f9298a3c53b483a0cfaf4c5ece37153fa
girmameb <girmamebratu@gmail.com>
2024-06-21 14:12:11 -0700

    update file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,10 @@
-number1== 10
-number2== 5
-print ("Addition of 10 and 5 is", number1+number2)
-print ("Subtraction of 10 and 5 is", number1-number2)
-print ("Multiplication of 10 and 5 is", number1*number2)
+number1= 10
+number2= 5
+r1=number1+number2
+r2=number1-number2
+r3=number1*number2
+print ("Addition of 10 and 5 is", r1)
+print ("Subtraction of 10 and 5 is", r2)
+print ("Multiplication of 10 and 5 is", r3)
+
 
