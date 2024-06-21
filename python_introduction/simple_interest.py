cbf89cdcccac79b6e9987acdc2e6beea69ad30e6
girmameb <girmamebratu@gmail.com>
2024-06-21 14:53:10 -0700

    update file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 #To calculate the simple interest 
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
+principal = 1000
+rate = 0.05
+time = 3
+I = principal * rate * time
 print ("The simple interest is :" , I)
