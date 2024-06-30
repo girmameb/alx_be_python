35ad75ef4516db65c1e7c2a0c0b9e4a5476c3cf5
girmameb <girmamebratu@gmail.com>
2024-06-30 01:12:49 -0700

    update file

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,13 @@
+# Prompt user for input
+size = int(input("Enter the size of the pattern: "))
+
+# Draw the pattern
+row = 0
+while row < size:
+    col = 0
+    while col < size:
+        print("*", end="")
+        col += 1
+    print() 
+    row += 1
+
