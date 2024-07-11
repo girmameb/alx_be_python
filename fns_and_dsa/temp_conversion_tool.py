bd416398832985ee495a292f38569deba04c076d
girmameb <girmamebratu@gmail.com>
2024-07-11 10:59:41 -0700

    file update

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,9 +2,6 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-FAHRENHEIT_TO_CELSIUS_OFFSET = 32
-CELSIUS_TO_FAHRENHEIT_OFFSET = 32
-
 def convert_to_celsius(fahrenheit):
     """
     Converts a temperature from Fahrenheit to Celsius.
@@ -13,11 +10,13 @@ def convert_to_celsius(fahrenheit):
     Returns:
     - float: The temperature converted to Celsius.
     """
+    FAHRENHEIT_TO_CELSIUS_OFFSET = 32
     celsius = (fahrenheit - FAHRENHEIT_TO_CELSIUS_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 
 def convert_to_fahrenheit(celsius):
+    CELSIUS_TO_FAHRENHEIT_OFFSET = 32
     """
     Converts a temperature from Celsius to Fahrenheit.
 
