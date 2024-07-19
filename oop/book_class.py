86171987eef6a4a2763772c03e7a0b9e18ebb23d
girmameb <girmamebratu@gmail.com>
2024-07-19 10:46:27 -0700

    update file

--- oop/book_class.py
+++ oop/book_class.py
@@ -14,7 +14,7 @@ class Book:
 
     def __str__(self):
         """Return a human-readable string representation of the Book object."""
-        return f"'{self.title}' by {self.author}, published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
         """Return an official string representation of the Book object."""
