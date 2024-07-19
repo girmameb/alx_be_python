d31b2b0705d1bf2535e40e98bc74288b504fb0b8
girmameb <girmamebratu@gmail.com>
2024-07-19 11:39:07 -0700

    update file

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -12,7 +12,7 @@ class Calculator:
     @classmethod
     def multiply(cls, a, b):
         """Class method to return the product of two numbers and print the class attribute calculation_type."""
-        print(f"Calculation Type: {cls.calculation_type}")
+        print(f"Calculation type: {cls.calculation_type}")
         return a * b
 
 
